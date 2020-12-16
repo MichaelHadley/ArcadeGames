616ff591257d1f73ed38007f4a7363dda8b3309c
MichaelHadley <michael.hadley1@live.co.uk>
2020-12-16 14:21:27 +0000

    First commit

---
+++ Assets/Scripts/Frogger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Frogger : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject Truck;
+    public GameObject player;
+    public Transform carStart;
+    public Transform carEnd;
+
+    public List<GameObject> Vehicles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vehicles.Add(car);
+        Vehicles.Add(Truck);
+
+        car = Instantiate(car, carStart.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
