594885062c2d3bade7f4f0066745b01d1dd6f502
James Hadley <james.hadley1@live.co.uk>
2020-12-22 17:58:39 +0000

    Basic movement added to player

---
+++ Assets/Scripts/SpaceInvaders/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Player;
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Player.transform.position += transform.right * (movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Player.transform.position -= transform.right * (movementSpeed * Time.deltaTime);
+        }
+    }
+}
