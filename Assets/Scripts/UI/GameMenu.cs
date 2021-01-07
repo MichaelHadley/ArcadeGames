c1206f57f28c2e0f440ac84e962f2f8dfceade3d
MichaelHadley <michael.hadley1@live.co.uk>
2021-01-07 19:43:08 +0000

    UI Updates

--- Assets/Scripts/UI/GameMenu.cs
+++ Assets/Scripts/UI/GameMenu.cs
@@ -33,20 +33,11 @@ public class GameMenu : MonoBehaviour
         SceneManager.LoadScene("SpaceInvadersMenu");
     }
 
-<<<<<<< HEAD
-    //Quits frogger and returns player to start menu
-    public void QuitFrogger()
-=======
     public void QuitSpaceInvaders()
->>>>>>> 90c8c377ac83838bf5897b93ff9ccddae28211f4
     {
         SceneManager.LoadScene("GameMenu");
     }
 
-<<<<<<< HEAD
-    //Close down game
-=======
->>>>>>> 90c8c377ac83838bf5897b93ff9ccddae28211f4
     public void QuitGame()
     {
         Application.Quit();
