78e9e3b78d99a6af7ae66a19cc93b6833d32f801
JHadley98 <james.hadley1@live.co.uk>
2021-01-28 18:32:00 +0000

    Enemy movement

--- Assets/Scripts/GameManagement/SoundManager.cs
+++ Assets/Scripts/GameManagement/SoundManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Add Audio Clips
-    public AudioClip PlayerBullet;
-    public AudioClip EnemyBullet;
-    public AudioClip Explosion;
+    // All sound effects in the game
+    // All are public so you can set them in the Inspector
+    public AudioClip playerBullet;
+    public AudioClip enemyBullet;
+    public AudioClip shipExplosion;
+    public AudioClip enemyDies;
+    public AudioClip enemyNoise1;
+    public AudioClip enemyNoise2;
 
+    // Refers to the audio source added to the SoundManager
+    // to play sound effects
     private AudioSource source;
 
+    // Holds the single instance of the SoundManager that 
+    // you can access from any script
     private static SoundManager _instance;
     public static SoundManager Instance
     {
@@ -29,6 +37,9 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // This is a singleton that makes sure you only
+        // ever have one Sound Manager
+        // If there is any other Sound Manager created destroy it
         if (_instance == null)
         {
             _instance = this;
@@ -42,6 +53,7 @@ public class SoundManager : MonoBehaviour
         source = audioSource;
     }
 
+    // Other GameObjects can call this to play sounds
     public void PlayOneShot(AudioClip clip)
     {
         source.PlayOneShot(clip);
---
+++ Assets/Scripts/SpaceInvaders/Enemy/EnemyManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public bool reachedBarrier;
+    public bool moveDown;
+    public int direction = 1;
+
+    public int numOfEnemies = 10;
+    public float speedMultiplier = 1f;
+
+    private static EnemyManager _instance;
+    public static EnemyManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<EnemyManager>();
+                _instance.Inited = false;
+            }
+            return _instance;
+        }
+    }
+    public bool Inited { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reachedBarrier)
+        {
+            reachedBarrier = false;
+
+            // Move enemies down when barrier is reached
+            moveDown = true;
+
+            // Reverse direction
+            direction = -direction;
+        }
+        else
+        {
+            moveDown = false;
+        }
+
+        if (numOfEnemies < 2)
+        {
+            speedMultiplier = 10f;
+        }
+        else if (numOfEnemies < 4)
+        {
+            speedMultiplier = 5f;
+        }
+        else if (numOfEnemies < 5)
+        {
+            speedMultiplier = 2f;
+        }
+
+    }
+}
