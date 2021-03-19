8c9275205bffbd6468c9104a2c712cd046bac186
James Hadley <james.hadley1@live.co.uk>
2021-03-19 11:43:15 +0000

    Bug fixed - when game over in space invaders and quit was pressed, the games would freeze, fixed by resetting time scale when quit button is pressed

--- Assets/Scripts/UI/GameOver.cs
+++ Assets/Scripts/UI/GameOver.cs
@@ -8,11 +8,14 @@ public class GameOver : MonoBehaviour
     public void LoadNewGame()
     {
         SceneManager.LoadScene("SpaceInvaders");
+        // Reset game time
         Time.timeScale = 1f;
     }
 
     public void QuitToMenu()
     {
         SceneManager.LoadScene("SpaceInvadersMenu");
+        // Reset game time
+        Time.timeScale = 1f;
     }
 }
